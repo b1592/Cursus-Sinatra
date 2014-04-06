7fc34d8544c7fec17ef47f299b5dc5e123db6d5b
Boris Ponsioen <boris1592@gmail.com>
2014-04-06 11:38:56 +0200

    Blogsysteem met db gemaakt, nog geen update en destroy

---
+++ application.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'data_mapper'
+
+# need install dm-sqlite-adapter
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Post
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :body, Text
+  property :created_at, DateTime
+  property :author, String
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Post.auto_upgrade!
+
+get '/' do
+  @posts = Post.all
+  erb :home
+end
+
+get '/over' do
+  erb :over
+end
+
+get '/contact' do
+  erb :contact
+end
+
+
+get '/style.css' do 
+  scss :"/scss/style"
+end
